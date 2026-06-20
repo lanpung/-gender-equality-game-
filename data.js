74769a6f310f4143d60148d941a0459ef6553b54
lanpung <0025410299@student.dthu.edu.vn>
2026-06-20 14:22:14 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,76 @@
+const vocab = [
+
+{
+word:"Equality",
+meaning:"Sự bình đẳng",
+ipa:"/ɪˈkwɒləti/"
+},
+
+{
+word:"Opportunity",
+meaning:"Cơ hội",
+ipa:"/ˌɒpəˈtjuːnəti/"
+},
+
+{
+word:"Respect",
+meaning:"Sự tôn trọng",
+ipa:"/rɪˈspekt/"
+},
+
+{
+word:"Responsibility",
+meaning:"Trách nhiệm",
+ipa:"/rɪˌspɒnsəˈbɪləti/"
+},
+
+{
+word:"Stereotype",
+meaning:"Định kiến",
+ipa:"/ˈsteriətaɪp/"
+},
+
+{
+word:"Participate",
+meaning:"Tham gia",
+ipa:"/pɑːˈtɪsɪpeɪt/"
+},
+
+{
+word:"Encourage",
+meaning:"Khuyến khích",
+ipa:"/ɪnˈkʌrɪdʒ/"
+}
+
+];
+
+const allMeanings = [
+"Sự bình đẳng",
+"Cơ hội",
+"Sự tôn trọng",
+"Trách nhiệm",
+"Định kiến",
+"Tham gia",
+"Khuyến khích"
+];
+
+const goodMessages = [
+
+"🌟 Excellent!",
+"🎉 Amazing!",
+"🦄 Fantastic!",
+"💖 Great Job!",
+"✨ Wonderful!",
+"🌸 Super!",
+"🍭 Brilliant!"
+];
+
+const badMessages = [
+
+"🌼 Don't give up!",
+"💛 Try again next time!",
+"🧁 You can do it!",
+"🌈 Keep learning!",
+"🐻 Nice try!",
+"🌷 Practice makes perfect!"
+];
